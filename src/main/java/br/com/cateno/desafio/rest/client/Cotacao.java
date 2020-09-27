fa05e619e169e6724d8cf6b9666200b37e91fca3
acarlos <acarlos.mendesjr@gmail.com>
2020-09-27 06:26:22 -0300

    Swagger

--- src/main/java/br/com/cateno/desafio/rest/client/Cotacao.java
+++ src/main/java/br/com/cateno/desafio/rest/client/Cotacao.java
@@ -1,15 +1,14 @@
 package br.com.cateno.desafio.rest.client;
 
 import java.util.List;
-import java.math.BigDecimal;
-import java.io.Serializable;
 
 public class Cotacao {
 
     public String context;
     public List<TipoCotacaoDolar> value;
 
-    public static class TipoCotacaoDolar implements Serializable{
+
+    public static class TipoCotacaoDolar {
         public String cotacaoCompra;
         public String cotacaoVenda;
         public String dataHoraCotacao;
